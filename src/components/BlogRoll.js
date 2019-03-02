2d15585ae68af84edf992a7ffa6d4ba601cd1b53
Sarah <sarah.ann.nicholson@gmail.com>
2019-03-02 15:43:07 -0800

    BlogRoll - Adding Featured image to card

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -7,35 +7,43 @@ class BlogRoll extends React.Component {
   render() {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
-    
+
     return (
       <div className="columns is-multiline">
-      {posts && (posts
+        {posts && (posts
           .map(({ node: post }) => (
-            <div
-              className="is-parent column is-6"
-              key={post.id}
-            >
-            <article className="tile is-child box notification">
-              <p>
-                <Link className="title has-text-primary is-size-4" to={post.fields.slug}>
-                  {post.frontmatter.title}
-                </Link>
-                <span> &bull; </span>
-                <span className="subtitle is-size-5 is-block">{post.frontmatter.date}</span>
-              </p>
-              <p>
-                {post.excerpt}
-                <br />
-                <br />
-                <Link className="button" to={post.fields.slug}>
-                  Keep Reading →
-                </Link>
-              </p>
-              </article>
+            <div className="is-parent column is-6" key={post.id}>
+              <div class="card notification">
+                <div class="card-image">
+                  <figure class="image">
+                    <img src={post.frontmatter.thumbnail} />
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p>
+                        <Link className="title has-text-primary is-size-4" to={post.fields.slug}>
+                          {post.frontmatter.title}
+                        </Link>
+                        <span> &bull; </span>
+                        <span className="subtitle is-size-5 is-block">{post.frontmatter.date}</span>
+                      </p>
+                      <p>
+                        {post.excerpt}
+                        <br />
+                        <br />
+                        <Link className="button" to={post.fields.slug}>
+                          View Recipe →
+                    </Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           )))}
-          </div>
+      </div>
     );
   }
 }
@@ -47,6 +55,34 @@ BlogRoll.propTypes = {
     }),
   }),
 }
+/*
+<div className="is-parent column is-6" key={post.id}>
+        <article className="tile is-child box notification">
+          <div className="img-container">
+            <img src={post.frontmatter.thumbnail} />
+          </div>
+          <div className="body-container">
+            <p>
+              <Link className="title has-text-primary is-size-4" to={post.fields.slug}>
+                {post.frontmatter.title}
+              </Link>
+              <span> &bull; </span>
+              <span className="subtitle is-size-5 is-block">{post.frontmatter.date}</span>
+            </p>
+            <p>
+              {post.excerpt}
+              <br />
+              <br />
+              <Link className="button" to={post.fields.slug}>
+                View Recipe →
+    </Link>
+            </p>
+          </div>
+        </article>
+      </div>
+      */
+
+
 
 export default () => (
   <StaticQuery
@@ -67,6 +103,7 @@ export default () => (
               title
               templateKey
               date(formatString: "MMMM DD, YYYY")
+              thumbnail
             }
           }
         }
